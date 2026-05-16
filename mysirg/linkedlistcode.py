58c00adeefd704930d029b6e3471b3f2109a09c9
Krishu-1709 <Krishnacodes007@gmail.com>
2026-05-16 18:37:18 +0530

    i have fucked up my dsa journey but its fine.we are going to go okaye

---
+++ mysirg/linkedlistcode.py
@@ -0,0 +1,15 @@
+class node:
+    def __init__(self,item = None,next = None) :
+        self.item = item
+        self.next = next
+class SLL:
+    def __init__(self,start = None):
+        self.start = start
+    def is_empty(self):
+        return self.start == None
+    def insert_at_start(self,data):
+        n = node(data,self.start)
+
+        
+
+my_list = SLL()
---
+++ raghavsirgfgjavadsa/String.java
@@ -0,0 +1,6 @@
+public class String{
+    public static void main(String[] args) {
+        char[] arr = {'K','r','i'};
+        
+    }
+}
